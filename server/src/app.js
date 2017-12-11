5c1a96c0d22002e49cea151cd86376a41a66f155
galangarbis <galangarbis@gmail.com>
2017-12-11 22:05:05 +0700

    Setting up the registration page

--- server/src/app.js
+++ server/src/app.js
@@ -8,9 +8,15 @@ app.use(morgan('combine'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+// app.get('/status', (req, res) => {
+//   res.send({
+//     message: 'hello world!'
+//   })
+// })
+
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: `Hello ${req.body.email}!your user was registered! have fun!`
   })
 })
 
