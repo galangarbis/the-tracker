8e38b177c70ee613539b94b37b3e52996a9d63e3
galangarbis <galangarbis@gmail.com>
2017-12-16 21:59:16 +0700

    Get Song Detail

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -1,11 +1,85 @@
 <template>
+  <v-layout>
+    <v-flex xs6>
+      <panel title="Song Metadata">
+        <v-layout>
+          <v-flex xs6>
+            <div class="song-title">
+              {{song.title}}
+            </div>
+            <div class="song-artist">
+              {{song.artist}}
+            </div>
+            <div class="song-genre">
+              {{song.genre}}
+            </div>
+          </v-flex>
+          <v-flex xs6>
+            <img class="album-image" :src="song.albumImageUrl" />
+            <br>
+            {{song.album}}
+          </v-flex>
+        </v-layout>
+      </panel>
+    </v-flex>
+    <v-flex xs6 class="ml-2">
+        <panel title="Tabs">
+            <textarea
+            readonly
+            v-model="song.tab"
+            ></textarea>
+        </panel>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-export default {
-}
+  import SongsService from '@/services/SongsService'
+  import Panel from '@/components/Panel'
+  export default {
+    data() {
+      return {
+        song: {}
+      }
+    },
+    async mounted() {
+      const songId = this.$store.state.route.params.songId
+      this.song = (await SongsService.show(songId)).data
+    },
+    components: {
+      Panel
+    }
+  }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.song {
+    padding: 20px;
+    height: 330px;
+    overflow: hidden;
+}
+.song-title {
+    font-size: 30px;
+}
+.song-artist {
+    font-size: 24px;    
+}
+.song-genre {
+    font-size: 18px;
+}
+.album-image {
+    width: 70%;
+    margin: 0 auto;
+}
+textarea {
+    width: 100%;
+    font-family: monospace;
+    border: none;
+    height: 500px;
+    border-style: none;
+    border-color: transparent;
+    overflow: auto;
+}
 </style>
